9e872f508b4eb66e9e0d676bd1d264f25a9ee9fb
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-02-08 17:25:54 -0300

    Reutilizei código de tarefas passadas para trazer um esqueleto base para utilizar a matriz de leds

--- tarefa-interfaces.c
+++ tarefa-interfaces.c
@@ -1,7 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 #include "hardware/pio.h"
-#include "hardware/timer.h"
-#include "hardware/clocks.h"
-#include "hardware/uart.h"
+#include "ws2812b.pio.h"
+
+#define LED_PIN 7
+#define BOTAO_A 5
+#define BOTAO_B 6
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+#define LED_R 13
+#define LED_G 11
+#define LED_B 12
+
+// Definição das variáveis globais para a PIO e máquina de estado 
+static PIO pio;
+static uint sm;
+
+// estrutura que armazena dados para um LED
+typedef struct{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+} led;
+
+// buffer dedicado aos LEDs e inicializa todos com 0
+volatile led matriz_led[LED_COUNT] = {0};
+
+// função que une os dados binários dos LEDs para emitir para a PIO
+uint32_t valor_rgb(uint8_t B, uint8_t R, uint8_t G){
+  return (G << 24) | (R << 16) | (B << 8);
+}
+
+// função que envia dados ao buffer de leds
+void set_led(uint8_t indice, uint8_t r, uint8_t g, uint8_t b){
+    if(indice < 25){
+    matriz_led[indice].R = r;
+    matriz_led[indice].G = g;
+    matriz_led[indice].B = b;
+    }
+}
+
+// função que limpa o buffer de leds
+void clear_leds(){
+    for(uint8_t i = 0; i < LED_COUNT; i++){
+        matriz_led[i].R = 0;
+        matriz_led[i].B = 0;
+        matriz_led[i].G = 0;
+    }
+}
+
+// função que manda os dados do buffer para os LEDs
+void print_leds(){
+    uint32_t valor;
+    for(uint8_t i = 0; i < LED_COUNT; i++){
+        valor = valor_rgb(matriz_led[i].B, matriz_led[i].R,matriz_led[i].G);
+        pio_sm_put_blocking(pio, sm, valor);
+    }
+}
+
+void config(){
+
+}
+
+int main(){
+    
+    config();
+    // configuração da PIO para a matriz de leds
+    pio = pio0; 
+    set_sys_clock_khz(128000, false);
+    uint offset = pio_add_program(pio, &ws2812b_program);
+    sm = pio_claim_unused_sm(pio, true);
+    ws2812b_program_init(pio, sm, offset, LED_PIN);
+
+    while(true){
+
+    }
+}
