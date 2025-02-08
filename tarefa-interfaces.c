d819eaf3bd153f482f5e2b973a353e890b858e1b
DaviErlon <163437822+DaviErlon@users.noreply.github.com>
2025-02-08 16:42:35 -0300

    Adição do esqueleto base para a contrução do código

---
+++ tarefa-interfaces.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
